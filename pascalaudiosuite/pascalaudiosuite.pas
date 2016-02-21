486d60cabb81d8e85786cb01d34de8db74266525
Andrew Haines <andrewd207@aol.com>
2016-02-21 09:31:50 -0500

    Added sox to package file

--- pascalaudiosuite/pascalaudiosuite.pas
+++ pascalaudiosuite/pascalaudiosuite.pas
@@ -9,7 +9,7 @@ interface
 uses
   pa_base, pa_lists, pa_resample, pa_enc_oggvorbis, pa_stream, pa_ladspa, 
   pa_dec_oggvorbis, pa_noiseremoval, pa_cdaudio, pa_process, pa_wav, 
-  pa_binaural, pa_samplerate, pa_pulse_simple, LazarusPackageIntf;
+  pa_binaural, pa_samplerate, pa_pulse_simple, pa_sox, LazarusPackageIntf;
 
 implementation
 
